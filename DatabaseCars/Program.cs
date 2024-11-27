7dddc8b9169296b0c0728ba90377a3d19eeff448
Klaimmor <arslanova699@gmail.com>
2024-11-27 19:41:21 +0500

    Добавьте файлы проекта.

---
+++ DatabaseCars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data.Entity;
+
+namespace DatabaseCars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
